468b9c201fb6845f8ad68f5e2c3bfa29861dbe0c
Milad Irannejad <moorara@users.noreply.github.com>
2025-02-06 16:22:37 -0500

    Implement LR parsing algorithms

--- internal/ebnf/parser/parser.go
+++ internal/ebnf/parser/parser.go
@@ -1,9 +1,16 @@
 package parser
 
 import (
+	"errors"
+	"fmt"
 	"io"
 
+	"github.com/moorara/algo/generic"
+	"github.com/moorara/algo/grammar"
 	"github.com/moorara/algo/lexer"
+	"github.com/moorara/algo/list"
+	"github.com/moorara/algo/parser"
+	"github.com/moorara/algo/parser/lr"
 
 	ebnflexer "github.com/gardenbed/emerge/internal/ebnf/lexer"
 )
@@ -25,4 +32,210 @@ func New(filename string, src io.Reader) (*Parser, error) {
 	return &Parser{
 		L: L,
 	}, nil
+}
+
+// nextToken wraps the Lexer.NextToken method and ensures
+// an Endmarker token is returned when the end of input is reached.
+func (p *Parser) nextToken() (lexer.Token, error) {
+	token, err := p.L.NextToken()
+	if err != nil && errors.Is(err, io.EOF) {
+		token.Terminal, token.Lexeme = grammar.Endmarker, ""
+		return token, nil
+	}
+
+	return token, err
+}
+
+// Parse implements the LR parsing algorithm.
+// It analyzes a sequence of input tokens (terminal symbols) provided by a lexical analyzer.
+// It attempts to parse the input according to the production rules of the EBNF grammar.
+//
+// The Parse method invokes the provided functions each time a token or a production rule is matched.
+// This allows the caller to process or react to each step of the parsing process.
+//
+// An error is returned if the input fails to conform to the grammar rules, indicating a syntax issue,
+// or if any of the provided functions return an error, indicating a semantic issue.
+func (p *Parser) Parse(tokenF parser.TokenFunc, prodF parser.ProductionFunc) error {
+	stack := list.NewStack[int](1024, generic.NewEqualFunc[int]())
+	stack.Push(0)
+
+	// Read the first input token.
+	token, err := p.nextToken()
+	if err != nil {
+		return &parser.ParseError{Cause: err}
+	}
+
+	for {
+		s, _ := stack.Peek()
+		a := token.Terminal
+
+		action, param, err := ACTION(s, a)
+		if err != nil {
+			return &parser.ParseError{
+				Description: fmt.Sprintf("unexpected string %q", token.Lexeme),
+				Cause:       err,
+				Pos:         token.Pos,
+			}
+		}
+
+		switch action {
+		case lr.SHIFT:
+			stack.Push(param)
+
+			// Yield the token.
+			if tokenF != nil {
+				if err := tokenF(&token); err != nil {
+					return &parser.ParseError{
+						Cause: err,
+						Pos:   token.Pos,
+					}
+				}
+			}
+
+			// Read the next input token.
+			token, err = p.nextToken()
+			if err != nil {
+				return &parser.ParseError{Cause: err}
+			}
+
+		case lr.REDUCE:
+			prod := productions[param]
+			A, β := prod.Head, prod.Body
+
+			for range len(β) {
+				stack.Pop()
+			}
+
+			// An LR parser detects an error when it consults the ACTION table.
+			// Errors are never identified by consulting the GOTO table.
+			// If ACTION(s, a) is not an error entry, GOTO(t, A) will also not be an error entry.
+
+			t, _ := stack.Peek()
+			next := GOTO(t, A)
+			stack.Push(next)
+
+			// Yield the production.
+			if prodF != nil {
+				if err := prodF(prod); err != nil {
+					return &parser.ParseError{Cause: err}
+				}
+			}
+
+		case lr.ACCEPT:
+			// Accept the input string.
+			return nil
+
+		case lr.ERROR:
+			// TODO: This is unreachable currently, since T.ACTION handles the error.
+		}
+	}
+}
+
+// ParseAndBuildAST implements the LR parsing algorithm.
+// It analyzes a sequence of input tokens (terminal symbols) provided by a lexical analyzer.
+// It attempts to parse the input according to the production rules of the EBNF grammar.
+//
+// If the input string is valid, the root node of the AST is returned,
+// representing the syntactic structure of the input string.
+//
+// An error is returned if the input fails to conform to the grammar rules, indicating a syntax issue.
+func (p *Parser) ParseAndBuildAST() (parser.Node, error) {
+	// Stack for constructing the abstract syntax tree.
+	nodes := list.NewStack[parser.Node](1024, parser.EqNode)
+
+	err := p.Parse(
+		func(token *lexer.Token) error {
+			nodes.Push(&parser.LeafNode{
+				Terminal: token.Terminal,
+				Lexeme:   token.Lexeme,
+				Position: token.Pos,
+			})
+
+			return nil
+		},
+		func(prod *grammar.Production) error {
+			in := &parser.InternalNode{
+				NonTerminal: prod.Head,
+				Production:  prod,
+			}
+
+			for range len(prod.Body) {
+				child, _ := nodes.Pop()
+				in.Children = append([]parser.Node{child}, in.Children...) // Maintain correct production body order
+			}
+
+			nodes.Push(in)
+
+			return nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// The nodes stack only contains the root of AST at this point.
+	root, _ := nodes.Pop()
+
+	return root, nil
+}
+
+// ParseAndEvaluate implements the LR parsing algorithm.
+// It analyzes a sequence of input tokens (terminal symbols) provided by a lexical analyzer.
+// It attempts to parse the input according to the production rules of the EBNF grammar.
+//
+// During the parsing process, the provided EvaluateFunc is invoked each time a production rule is matched.
+// The function is called with values corresponding to the symbols in the body of the production,
+// enabling the caller to process and evaluate the input incrementally.
+//
+// An error is returned if the input fails to conform to the grammar rules, indicating a syntax issue,
+// or if the evaluation function returns an error, indicating a semantic issue.
+func (p *Parser) ParseAndEvaluate(eval lr.EvaluateFunc) (*lr.Value, error) {
+	// Stack for constructing the abstract syntax tree.
+	nodes := list.NewStack[*lr.Value](1024, nil)
+
+	err := p.Parse(
+		func(token *lexer.Token) error {
+			copy := token.Pos
+			nodes.Push(&lr.Value{
+				Val: token.Lexeme,
+				Pos: &copy,
+			})
+
+			return nil
+		},
+		func(prod *grammar.Production) error {
+			l := len(prod.Body)
+			rhs := make([]*lr.Value, l)
+
+			// Maintain correct production body order
+			for i := l - 1; i >= 0; i-- {
+				v, _ := nodes.Pop()
+				rhs[i] = v
+			}
+
+			lhs, err := eval(prod, rhs)
+			if err != nil {
+				return err
+			}
+
+			v := &lr.Value{Val: lhs}
+			if l > 0 {
+				v.Pos = rhs[0].Pos
+			}
+
+			nodes.Push(v)
+
+			return nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// The nodes stack only contains the root of AST at this point.
+	root, _ := nodes.Pop()
+
+	return root, nil
 }
