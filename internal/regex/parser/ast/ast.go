654887c13200815607d749c8854fd3f438a1f40d
Milad Irannejad <moorara@users.noreply.github.com>
2025-10-27 20:26:03 -0400

    Revamp regex/parser/ast

--- internal/regex/parser/ast/ast.go
+++ internal/regex/parser/ast/ast.go
@@ -5,12 +5,15 @@
 package ast
 
 import (
+	"errors"
 	"fmt"
 	"sort"
 
-	auto "github.com/moorara/algo/automata"
+	"github.com/moorara/algo/automata"
 	"github.com/moorara/algo/list"
+	"github.com/moorara/algo/parser/combinator"
 
+	"github.com/gardenbed/emerge/internal/regex/char"
 	"github.com/gardenbed/emerge/internal/regex/parser"
 )
 
@@ -26,11 +29,10 @@ const endMarker rune = 0xEEEE
 
 // AST is the abstract syntax tree for a regular expression.
 type AST struct {
-	Root Node
-
+	Root      Node
 	lastPos   Pos
-	posToChar map[Pos]rune
-	charToPos map[rune]Poses
+	posToChar map[Pos]char.Range
+	charToPos map[char.Range]Poses
 	follows   map[Pos]Poses
 }
 
@@ -52,65 +54,70 @@ func Parse(regex string) (*AST, error) {
 	root := &Concat{
 		Exprs: []Node{
 			out.Result.Val.(Node),
-			&Char{Val: endMarker},
+			&Char{Lo: endMarker, Hi: endMarker},
 		},
 	}
 
 	a := &AST{
 		Root:      root,
 		lastPos:   0,
-		posToChar: map[Pos]rune{},
-		charToPos: map[rune]Poses{},
+		posToChar: map[Pos]char.Range{},
+		charToPos: map[char.Range]Poses{},
 		follows:   map[Pos]Poses{},
 	}
 
-	// Preprocessing: assign positions to Char nodes and index them
-	a.indexChars(a.Root)
+	// Assign one-based positions to Char nodes.
+	a.backfillCharPos(a.Root)
 
-	// Preprocessing: compute followpos function
+	// Preprocessing: compute followpos function.
 	a.computeFollows(a.Root)
-	for _, list := range a.follows {
-		sort.Sort(list)
+	for _, poses := range a.follows {
+		sort.Sort(poses)
 	}
 
 	return a, nil
 }
 
-// indexChars backfills Pos for all Char nodes in the abstract syntaxt tree from left to right.
+// backfillCharPos backfills Pos for all Char nodes in the abstract syntaxt tree from left to right.
 // These positions are one-based and used for directly converting a regular expression to a DFA.
 // They are semantically different from the zero-based positions set by parsers (in the mappers).
 //
 // It also creates a map of positions to characters and vice versa.
-func (a *AST) indexChars(n Node) {
+// These mappings are used when constructing the DFA from the AST.
+func (a *AST) backfillCharPos(n Node) {
 	switch v := n.(type) {
 	case *Concat:
 		for _, e := range v.Exprs {
-			a.indexChars(e)
+			a.backfillCharPos(e)
 		}
 
 	case *Alt:
 		for _, e := range v.Exprs {
-			a.indexChars(e)
+			a.backfillCharPos(e)
 		}
 
 	case *Star:
-		a.indexChars(v.Expr)
+		a.backfillCharPos(v.Expr)
 
 	case *Char:
+		// Assign the next position to the current Char.
 		a.lastPos++
-		p, c := a.lastPos, v.Val
-		v.Pos = p
-		a.posToChar[p] = c
-		a.charToPos[c] = append(a.charToPos[c], p)
+		v.Pos = a.lastPos
+
+		// Update the pos-to-char and char-to-pos mappings.
+		c := char.Range{v.Lo, v.Hi}
+		a.posToChar[v.Pos] = c
+		a.charToPos[c] = append(a.charToPos[c], v.Pos)
 	}
 }
 
 // There are only two ways that a position of a regular expression can be made to follow another.
 //
-// 1. If n is a concat node with left child n1 and right child n2,
-// then for every position i in lastPos(n1), all positions in firstPos(n2) are in followPos(i).
+//  1. If n is a concat node with left child n1 and right child n2,
+//     then for every position i in lastPos(n1), all positions in firstPos(n2) are in followPos(i).
 //
-// 2. If n is a star node, and i is a position in lastPos(n1), then all positions in firstPos(n1) are in followPos(i).
+//  2. If n is a star node, and i is a position in lastPos(n1),
+//     then all positions in firstPos(n1) are in followPos(i).
 func (a *AST) computeFollows(n Node) {
 	switch v := n.(type) {
 	case *Concat:
@@ -138,10 +145,9 @@ func (a *AST) computeFollows(n Node) {
 	}
 }
 
-// for a position p, is the set of positions q in the entire syntax tree
-// such that there is some string x = a1a2...an in L((r)µ)
-// such that for some i, there is a way to explain the membership of x in L((r)µ)
-// by matching ai to position p of the syntax tree and ai+1 to position q.
+// followPos for a position p, is the set of positions q in the entire syntax tree such that there is some string
+// x = a₁a₂...aₙ in L((r)µ) such that for some i, there is a way to explain the membership of x in L((r)µ) by
+// matching aᵢ to position p of the syntax tree and aᵢ₊₁ to position q.
 func (a *AST) followPos(p Pos) Poses {
 	return a.follows[p]
 }
@@ -149,8 +155,11 @@ func (a *AST) followPos(p Pos) Poses {
 // ToDFA converts the abstract syntax tree for a regular expression to a DFA.
 //
 // For more details, see Compilers: Principles, Techniques, and Tools (2nd Edition).
-func (a *AST) ToDFA() *auto.DFA {
-	dfa := auto.NewDFA(0, nil)
+func (a *AST) ToDFA() *automata.DFA {
+	end := char.Range{endMarker, endMarker}
+
+	b := automata.NewDFABuilder().SetStart(0)
+
 	Dstates := list.NewSoftQueue(func(p, q Poses) bool {
 		return p.Equal(q)
 	})
@@ -160,7 +169,7 @@ func (a *AST) ToDFA() *auto.DFA {
 
 	for S, i := Dstates.Dequeue(); i >= 0; S, i = Dstates.Dequeue() {
 		for c := range a.charToPos { // for each input symbol c
-			if c != endMarker {
+			if c != end {
 				// Let U be the union of followpos(p) for all p in S that correspond to c
 				U := Poses{}
 				for _, p := range S {
@@ -175,27 +184,24 @@ func (a *AST) ToDFA() *auto.DFA {
 					j = Dstates.Enqueue(U)
 				}
 
-				dfa.Add(auto.State(i), auto.Symbol(c), auto.State(j))
+				lo, hi := automata.Symbol(c[0]), automata.Symbol(c[1])
+				b.AddTransition(automata.State(i), lo, hi, automata.State(j))
 			}
 		}
 	}
 
-	dfa.Start = auto.State(0)
-	dfa.Final = auto.NewStates()
+	final := []automata.State{}
 
 	for i, S := range Dstates.Values() {
-		for _, f := range a.charToPos[endMarker] {
+		for _, f := range a.charToPos[end] {
 			if S.Contains(f) {
-				dfa.Final.Add(auto.State(i))
+				final = append(final, automata.State(i))
 				break // The accepting states of D are all those sets of positions that include the position of the end-marker
 			}
 		}
 	}
 
-	// Minimize the no. of states
-	dfa = dfa.Minimize()
-
-	return dfa
+	return b.SetFinal(final).Build().Minimize()
 }
 
 // Node is the interface for all nodes in an abstract syntax tree.
@@ -210,6 +216,9 @@ type Node interface {
 	// lastPos(n) is the set of positions in the subtree rooted at n that
 	// correspond to the last symbol of at least one string in the language of the subexpression rooted at n.
 	lastPos() Poses
+
+	// equal checks if two nodes are equal.
+	equal(Node) bool
 }
 
 type computed struct {
@@ -230,7 +239,7 @@ func (n *Concat) compute() {
 	}
 
 	n.comp = &computed{
-		nullable: false,
+		nullable: true,
 		firstPos: Poses{},
 		lastPos:  Poses{},
 	}
@@ -267,6 +276,25 @@ func (n *Concat) firstPos() Poses {
 func (n *Concat) lastPos() Poses {
 	n.compute()
 	return n.comp.lastPos
+}
+
+func (n *Concat) equal(rhs Node) bool {
+	nn, ok := rhs.(*Concat)
+	if !ok {
+		return false
+	}
+
+	if len(n.Exprs) != len(nn.Exprs) {
+		return false
+	}
+
+	for i := 0; i < len(n.Exprs); i++ {
+		if !n.Exprs[i].equal(nn.Exprs[i]) {
+			return false
+		}
+	}
+
+	return true
 }
 
 // Alt represents an Alternation node.
@@ -308,6 +336,25 @@ func (n *Alt) lastPos() Poses {
 	return n.comp.lastPos
 }
 
+func (n *Alt) equal(rhs Node) bool {
+	nn, ok := rhs.(*Alt)
+	if !ok {
+		return false
+	}
+
+	if len(n.Exprs) != len(nn.Exprs) {
+		return false
+	}
+
+	for i := 0; i < len(n.Exprs); i++ {
+		if !n.Exprs[i].equal(nn.Exprs[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
 // Star represents a Kleene Star node.
 type Star struct {
 	Expr Node
@@ -325,6 +372,15 @@ func (n *Star) lastPos() Poses {
 	return n.Expr.lastPos()
 }
 
+func (n *Star) equal(rhs Node) bool {
+	nn, ok := rhs.(*Star)
+	if !ok {
+		return false
+	}
+
+	return n.Expr.equal(nn.Expr)
+}
+
 // Empty represents an Empty string ε leaf node.
 type Empty struct{}
 
@@ -340,9 +396,17 @@ func (n *Empty) lastPos() Poses {
 	return Poses{}
 }
 
+func (n *Empty) equal(rhs Node) bool {
+	_, ok := rhs.(*Empty)
+
+	return ok
+}
+
 // Char represents a Character leaf node.
+// It represents an inclusive range of characters.
 type Char struct {
-	Val rune
+	Lo  rune
+	Hi  rune
 	Pos Pos // one-based
 }
 
@@ -356,6 +420,15 @@ func (n *Char) firstPos() Poses {
 
 func (n *Char) lastPos() Poses {
 	return Poses{n.Pos}
+}
+
+func (n *Char) equal(rhs Node) bool {
+	nn, ok := rhs.(*Char)
+	if !ok {
+		return false
+	}
+
+	return n.Lo == nn.Lo && n.Hi == nn.Hi && n.Pos == nn.Pos
 }
 
 // Pos is the type for positions.
@@ -413,4 +486,516 @@ func (p Poses) Union(q Poses) Poses {
 	}
 
 	return u
+}
+
+//==================================================< MAPPERS >==================================================
+
+// Anchor is the type for a regular expression anchor.
+type Anchor int
+
+const (
+	StartOfString Anchor = iota + 1
+	EndOfString
+
+	bagKeyCharRanges     combinator.BagKey = "char_ranges"
+	bagKeyLazyQuantifier combinator.BagKey = "lazy_quantifier"
+	BagKeyStartOfString  combinator.BagKey = "start_of_string"
+)
+
+// mappers implements the parser.Mappers interface.
+type mappers struct {
+	errors error
+}
+
+func (m *mappers) ToAnyChar(r combinator.Result) (combinator.Result, bool) {
+	node, _ := charRangesToNode(false, char.Classes["UNICODE"])
+
+	return combinator.Result{
+		Val: node,
+		Pos: r.Pos,
+	}, true
+}
+
+func (m *mappers) ToSingleChar(r combinator.Result) (combinator.Result, bool) {
+	c := r.Val.(rune)
+	node, ranges := charRangesToNode(false, char.RangeList{{c, c}})
+
+	return combinator.Result{
+		Val: node,
+		Pos: r.Pos,
+		Bag: combinator.Bag{
+			bagKeyCharRanges: ranges,
+		},
+	}, true
+}
+
+func (m *mappers) ToCharClass(r combinator.Result) (combinator.Result, bool) {
+	class := r.Val.(string)
+
+	var node Node
+	var ranges char.RangeList
+
+	switch class {
+	case `\s`:
+		node, ranges = charRangesToNode(false, char.Classes[`\s`])
+	case `\S`:
+		node, ranges = charRangesToNode(true, char.Classes[`\s`])
+	case `\d`:
+		node, ranges = charRangesToNode(false, char.Classes[`\d`])
+	case `\D`:
+		node, ranges = charRangesToNode(true, char.Classes[`\d`])
+	case `\w`:
+		node, ranges = charRangesToNode(false, char.Classes[`\w`])
+	case `\W`:
+		node, ranges = charRangesToNode(true, char.Classes[`\w`])
+	default:
+		return combinator.Result{}, false
+	}
+
+	return combinator.Result{
+		Val: node,
+		Pos: r.Pos,
+		Bag: combinator.Bag{
+			bagKeyCharRanges: ranges,
+		},
+	}, true
+}
+
+func (m *mappers) ToASCIICharClass(r combinator.Result) (combinator.Result, bool) {
+	class := r.Val.(string)
+
+	ranges, ok := char.Classes[class]
+	if !ok {
+		return combinator.Result{}, false
+	}
+
+	node, ranges := charRangesToNode(false, ranges)
+
+	return combinator.Result{
+		Val: node,
+		Pos: r.Pos,
+		Bag: combinator.Bag{
+			bagKeyCharRanges: ranges,
+		},
+	}, true
+}
+
+func (m *mappers) ToUnicodeCategory(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToUnicodeCharClass(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r2, _ := r.Get(2)
+
+	prop := r0.Val.(string)
+	class := r2.Val.(string)
+
+	ranges, ok := char.Classes[class]
+	if !ok {
+		return combinator.Result{}, false
+	}
+
+	node, ranges := charRangesToNode(prop == `\P`, ranges)
+
+	return combinator.Result{
+		Val: node,
+		Pos: r.Pos,
+		Bag: combinator.Bag{
+			bagKeyCharRanges: ranges,
+		},
+	}, true
+}
+
+func (m *mappers) ToRepOp(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToUpperBound(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+
+	var num *int
+	if v, ok := r1.Val.(int); ok {
+		num = &v
+	}
+
+	return combinator.Result{
+		Val: num,
+		Pos: r0.Pos,
+	}, true
+}
+
+func (m *mappers) ToRange(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+	r2, _ := r.Get(2)
+
+	// The upper bound is same as the lower bound if no upper bound is specified (default)
+	low := r1.Val.(int)
+	up := &low
+
+	// If an upper bound is specified, it can be either bounded or unbounded
+	if v, ok := r2.Val.(*int); ok {
+		up = v
+	}
+
+	if up != nil && low > *up {
+		// The input syntax is correct while its semantic is incorrect
+		// We continue parsing the rest of input to find more errors
+		m.errors = errors.Join(m.errors, fmt.Errorf("invalid repetition range {%d,%d}", low, *up))
+	}
+
+	return combinator.Result{
+		Val: tuple[int, *int]{
+			p: low,
+			q: up,
+		},
+		Pos: r0.Pos,
+	}, true
+}
+
+func (m *mappers) ToRepetition(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToQuantifier(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+
+	// Check whether or not the lazy modifier is present
+	_, lazy := r1.Val.(rune)
+
+	return combinator.Result{
+		Val: tuple[any, bool]{
+			p: r0.Val,
+			q: lazy,
+		},
+		Pos: r0.Pos,
+	}, true
+}
+
+func (m *mappers) ToCharInRange(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToCharRange(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r2, _ := r.Get(2)
+
+	lo, hi := r0.Val.(rune), r2.Val.(rune)
+
+	if lo > hi {
+		m.errors = errors.Join(m.errors, fmt.Errorf("invalid character range %s-%s", string(lo), string(hi)))
+
+		// The input syntax is correct while its semantic is incorrect.
+		// We continue parsing the rest of input to find more errors.
+		return combinator.Result{Pos: r0.Pos}, true
+	}
+
+	node, ranges := charRangesToNode(false, char.RangeList{{lo, hi}})
+
+	return combinator.Result{
+		Val: node,
+		Pos: r0.Pos,
+		Bag: combinator.Bag{
+			bagKeyCharRanges: ranges,
+		},
+	}, true
+}
+
+func (m *mappers) ToCharGroupItem(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToCharGroup(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+	r2, _ := r.Get(2)
+
+	// Check whether or not the negation modifier is present
+	_, neg := r1.Val.(rune)
+
+	// Collect all character ranges from the character group items
+	var all char.RangeList
+	for _, r := range r2.Val.(combinator.List) {
+		if ranges, ok := r.Bag[bagKeyCharRanges].(char.RangeList); ok {
+			all = append(all, ranges...)
+		}
+	}
+
+	node, _ := charRangesToNode(neg, all.Dedup())
+
+	return combinator.Result{
+		Val: node,
+		Pos: r0.Pos,
+	}, true
+}
+
+func (m *mappers) ToMatchItem(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToMatch(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+
+	node := r0.Val.(Node)
+	var bag combinator.Bag
+
+	if t, ok := r1.Val.(tuple[any, bool]); ok {
+		node = quantifyNode(node, t.p)
+		if lazy := t.q; lazy {
+			bag = combinator.Bag{
+				bagKeyLazyQuantifier: true,
+			}
+		}
+	}
+
+	return combinator.Result{
+		Val: node,
+		Pos: r0.Pos,
+		Bag: bag,
+	}, true
+}
+
+func (m *mappers) ToGroup(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+	r3, _ := r.Get(3)
+
+	node := r1.Val.(Node)
+	var bag combinator.Bag
+
+	if t, ok := r3.Val.(tuple[any, bool]); ok {
+		node = quantifyNode(node, t.p)
+		if lazy := t.q; lazy {
+			bag = combinator.Bag{
+				bagKeyLazyQuantifier: true,
+			}
+		}
+	}
+
+	return combinator.Result{
+		Val: node,
+		Pos: r0.Pos,
+		Bag: bag,
+	}, true
+}
+
+func (m *mappers) ToAnchor(r combinator.Result) (combinator.Result, bool) {
+	c := r.Val.(rune)
+
+	var anchor Anchor
+	switch c {
+	case '$': // end-of-string
+		anchor = EndOfString
+	}
+
+	return combinator.Result{
+		Val: anchor,
+		Pos: r.Pos,
+	}, true
+}
+
+func (m *mappers) ToSubexprItem(r combinator.Result) (combinator.Result, bool) {
+	// Passing the result up the parsing chain
+	return r, true
+}
+
+func (m *mappers) ToSubexpr(r combinator.Result) (combinator.Result, bool) {
+	items := r.Val.(combinator.List)
+
+	concat := new(Concat)
+	for _, r := range items {
+		// TODO: Anchor result value is not a node
+		if n, ok := r.Val.(Node); ok {
+			concat.Exprs = append(concat.Exprs, n)
+		}
+	}
+
+	return combinator.Result{
+		Val: concat,
+		Pos: r.Pos,
+	}, true
+}
+
+func (m *mappers) ToExpr(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+
+	node := r0.Val.(Node)
+
+	if _, ok := r1.Val.(combinator.List); ok {
+		r11, _ := r1.Get(1)
+		expr := r11.Val.(Node)
+		node = &Alt{
+			Exprs: []Node{node, expr},
+		}
+	}
+
+	return combinator.Result{
+		Val: node,
+		Pos: r0.Pos,
+	}, true
+}
+
+func (m *mappers) ToRegex(r combinator.Result) (combinator.Result, bool) {
+	r0, _ := r.Get(0)
+	r1, _ := r.Get(1)
+
+	pos := r1.Pos
+	var bag combinator.Bag
+
+	// Check whether or not the start-of-string is present
+	if _, sos := r0.Val.(rune); sos {
+		pos = r0.Pos
+		bag = combinator.Bag{
+			BagKeyStartOfString: true,
+		}
+	}
+
+	expr := r1.Val.(Node)
+
+	return combinator.Result{
+		Val: expr,
+		Pos: pos,
+		Bag: bag,
+	}, true
+}
+
+//==================================================< HELPERS >==================================================
+
+type tuple[P, Q any] struct {
+	p P
+	q Q
+}
+
+// charRangesToNode converts a list of character ranges into a Node.
+// The resulting Node is either a Char node or an Alt node.
+//
+// If neg is true, the resulting Alt node will represent the negation of the given ranges.
+// It also returns the list of rune ranges represented by the resulting Alt node.
+func charRangesToNode(neg bool, ranges char.RangeList) (Node, char.RangeList) {
+	if neg {
+		ranges = char.Classes["UNICODE"].Exclude(ranges)
+	}
+
+	if len(ranges) == 1 {
+		lo, hi := ranges[0][0], ranges[0][1]
+		return &Char{Lo: lo, Hi: hi /* Pos will be set later */}, ranges
+	}
+
+	alt := new(Alt)
+	for _, r := range ranges {
+		lo, hi := r[0], r[1]
+		alt.Exprs = append(alt.Exprs, &Char{Lo: lo, Hi: hi /* Pos will be set later */})
+	}
+
+	return alt, ranges
+}
+
+// quantifyNFA applies the quantifier q to the given node n and returns the resulting node.
+func quantifyNode(n Node, q any) Node {
+	var node Node
+
+	switch rep := q.(type) {
+	// Simple repetition
+	case rune:
+		switch rep {
+		case '?':
+			node = &Alt{
+				Exprs: []Node{
+					&Empty{},
+					cloneNode(n),
+				},
+			}
+
+		case '*':
+			node = &Star{
+				Expr: cloneNode(n),
+			}
+
+		case '+':
+			node = &Concat{
+				Exprs: []Node{
+					cloneNode(n),
+					&Star{
+						Expr: cloneNode(n),
+					},
+				},
+			}
+		}
+
+	// Range repetition
+	case tuple[int, *int]:
+		low, up := rep.p, rep.q
+		concat := new(Concat)
+
+		for i := 0; i < low; i++ {
+			concat.Exprs = append(concat.Exprs, cloneNode(n))
+		}
+
+		if up == nil {
+			concat.Exprs = append(concat.Exprs, &Star{
+				Expr: cloneNode(n),
+			})
+		} else {
+			for i := 0; i < *up-low; i++ {
+				concat.Exprs = append(concat.Exprs, &Alt{
+					Exprs: []Node{
+						&Empty{},
+						cloneNode(n),
+					},
+				})
+			}
+		}
+
+		node = concat
+	}
+
+	return node
+}
+
+// Cloning is required since each instance of Char will have a distinct value for Pos field.
+func cloneNode(n Node) Node {
+	switch v := n.(type) {
+	case *Concat:
+		concat := new(Concat)
+		for _, e := range v.Exprs {
+			concat.Exprs = append(concat.Exprs, cloneNode(e))
+		}
+		return concat
+
+	case *Alt:
+		alt := new(Alt)
+		for _, e := range v.Exprs {
+			alt.Exprs = append(alt.Exprs, cloneNode(e))
+		}
+		return alt
+
+	case *Star:
+		return &Star{
+			Expr: cloneNode(v.Expr),
+		}
+
+	case *Empty:
+		return new(Empty)
+
+	case *Char:
+		return &Char{
+			Lo:  v.Lo,
+			Hi:  v.Hi,
+			Pos: v.Pos,
+		}
+
+	default:
+		return nil
+	}
 }
