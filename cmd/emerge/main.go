e60aa93d28045b4469a38705c0887b502dbd47fd
Milad <moorara@users.noreply.github.com>
2025-11-19 01:13:05 -0500

    Refactor os.Exit call (#109)

--- cmd/emerge/main.go
+++ cmd/emerge/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 
+	// "net/http"
+	// _ "net/http/pprof"
+
 	"github.com/gardenbed/charm/flagit"
 	"github.com/gardenbed/charm/ui"
 
@@ -13,23 +16,36 @@ import (
 )
 
 func main() {
+	// Start pprof server.
+	// Visit http://localhost:6060/debug/pprof/
+	/* go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}() */
+
+	if code := run(); code != 0 {
+		os.Exit(code)
+	}
+}
+
+// run is the main entry point for the emerge command.
+func run() int {
 	u := ui.New(ui.Info)
 	fs := flag.NewFlagSet("emerge", flag.ContinueOnError)
 
 	cmd, err := command.New(u)
 	if err != nil {
 		u.Errorf(ui.Red, "%s", err)
-		os.Exit(1)
+		return 1
 	}
 
 	if err := flagit.Register(fs, cmd, false); err != nil {
 		u.Errorf(ui.Red, "%s", err)
-		os.Exit(1)
+		return 1
 	}
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		u.Errorf(ui.Red, "%s", err)
-		os.Exit(1)
+		return 1
 	}
 
 	// Update the verbosity level.
@@ -41,7 +57,7 @@ func main() {
 	case cmd.Help:
 		if err := cmd.PrintHelp(); err != nil {
 			u.Errorf(ui.Red, "%s", err)
-			os.Exit(1)
+			return 1
 		}
 
 	case cmd.Version:
@@ -50,9 +66,9 @@ func main() {
 	default:
 		if err := cmd.Run(fs.Args()); err != nil {
 			u.Errorf(ui.Red, "\n%s\n", err)
-			os.Exit(1)
+			return 1
 		}
 	}
 
-	os.Exit(0)
+	return 0
 }
